4cfccf37cdec98db7fc209ebdd3bda25ca798f1c
Adam Morsi <adam.morsi@yahoo.com>
2018-07-20 17:55:48 +0300

    After solving the assignment

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Course extends Component {
-    render () {
+    state = {
+        courseTitle: ''
+    };
+
+    componentDidMount() {
+        this.getTitle();
+    }
+
+    componentDidUpdate() {
+        this.getTitle();
+    }
+
+    getTitle() {
+        const queryPar = new URLSearchParams(this.props.location.search);
+        for (let param of queryPar.entries()) {
+            if(this.state.courseTitle !== param[1]){
+                this.setState({courseTitle: param[1]});
+            }
+        }
+    }
+
+
+    render() {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.state.courseTitle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
